54da11425b4d8d0ab1057ae2d1751664a602df2b
Pak <Pak@admins-imac-69.lan>
2018-10-04 11:38:01 -0400

    solution

--- abCheck.js
+++ abCheck.js
@@ -9,11 +9,35 @@ Use the Parameter Testing feature in the box below to test your code with differ
 
 */
 
-function abCheck(str) { 
 
 
+    function abCheck(str) {
 
-}
+        str = str.toLowerCase();
+        var counter = 0;
+      
+        for (let i = 0; i < str.length; i++) {
+          if (str[i].charCodeAt() === 97) {
+            if (str[i + 4] === 'b' || str[i - 4] === 'b') {
+              counter += 1;
+            }
+          } else if (str[i].charCodeAt() === 98) {
+             if (str[i + 4] === 'a' || str[i - 4] === 'a') {
+              counter += 1;
+            }
+          }
+        }
+      
+        if (counter > 0) {
+          return 'true';
+        } else {
+          return 'false';
+        }
+      
+      }
+      
+
+
 
 
 
